6ea8b6dbab7199951ff2a08ab272282b5fddc9e7
Yoshi <joseangelyoshi@gmail.com>
2020-08-06 15:53:28 +0200

    Basic files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,118 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  public entityArray = [
+        {'initiative': 15, 'name': 'Slime'},
+        {'initiative': 4, 'name': 'Vampire'},
+        {'initiative': 12, 'name': 'Archer thief'}
+      ];
+
+  timeoutHandler: number;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
+
+
+  public editEntity(currentEntity, index) {
+    this.alertCtrl.create({
+      subHeader: 'Edit entity',
+      message: 'Current: "' + currentEntity.name + '"',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: currentEntity.name
+        },
+        {
+          name: 'initiative',
+          placeholder: 'Initiative',
+          type: 'number',
+          value: currentEntity.initiative
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            if (data.name.length != 0 && data.initiative > 0) {
+              this.entityArray[index] = {'initiative': data.initiative, 'name': data.name};
+              this.entityArray.sort((a,b) => b.initiative - a.initiative);
+            }
+          }
+        }
+      ]
+    }).then(alert => alert.present());
+  };
+
+  public addEntity() {
+    let index = this.entityArray.length;
+    this.alertCtrl.create({
+      subHeader: 'Add new entity',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+        },
+        {
+          name: 'initiative',
+          placeholder: 'Initiative',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            if (data.name.length != 0 && data.initiative > 0) {
+              this.entityArray[index] = {'initiative': data.initiative, 'name': data.name};
+              this.entityArray.sort((a,b) => b.initiative - a.initiative);
+            }
+          }
+        }
+      ]
+    }).then(alert => alert.present());
+  }
+
+  public roll(maxValue) {
+    this.alertCtrl.create({
+      subHeader: 'D' + maxValue,
+      cssClass: 'dice-alert',
+      message: Math.floor(Math.random() * maxValue + 1).toString()
+    }).then(alert => alert.present());
+  }
+
+  public deleteEntity(index) {
+    this.alertCtrl.create({
+      message: 'Do you want to delete this entity?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: () => {
+            this.entityArray.splice(index, 1);
+            this.entityArray.sort((a,b) => b.initiative - a.initiative);
+          }
+        }
+      ]
+    }).then(alert => alert.present());
+  }
+
 
 }
